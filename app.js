51a398aac1f6ae447e167ee2b772d7cb2fe3c5bf
reubengt <reubengeorgethomas@gmail.com>
2019-03-16 09:25:35 +0000

    animate button in after typing finishes

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
+//variable to store scrollbtn
+let scrollbtn=document.querySelector(".scrollbtn");
 //variable to store each p tag in the code window
 let codelines=[...document.querySelectorAll(".code-window p")];
 //creates a copy of the previous array, but with the elements replaced with the values of their text contents
 //this will be used later to re-fill the empty elements
 let codelinesCopy=codelines.map(a => a.textContent);
-
 //setting an interval time for each line to be the length of the longest line multiplied by 80ms(the interval for each letter)
 let intervalTime=0;
 //resets the innerText to be blank
@@ -26,6 +27,9 @@ function typeline() {
   //recursively calls the function for the next line with a timeout interval slightly greater than the time taken to display the previous line
   if(lineIndex<codelines.length){
     setTimeout(typeline, intervalTime);
+  }
+  else{
+    scrollbtn.style.opacity = "1.0";
   }
 }
 typeline();
